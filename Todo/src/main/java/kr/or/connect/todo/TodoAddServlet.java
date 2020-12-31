960d3bd514ef2d206135fe85d4feebd60f45eacd
Naeun <sne12345@naver.com>
2021-01-01 04:02:50 +0900

    commit

--- Todo/src/main/java/kr/or/connect/todo/TodoAddServlet.java
+++ Todo/src/main/java/kr/or/connect/todo/TodoAddServlet.java
@@ -15,7 +15,7 @@ public class TodoAddServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     public TodoAddServlet() {
-        super(); 
+        super();  
         
     }
 
